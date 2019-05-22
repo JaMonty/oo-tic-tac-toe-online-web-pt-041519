e84e2875e36841a1d690bd7c64c2e0bfae120af9
Jason  Montes <jasone.montes@gmail.com>
2019-05-22 03:39:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,7 +105,19 @@ end
     end
   end  
   
-  
+  def play
+    until over?
+      turn
+    end
+
+     if won?
+      puts "Congratulations #{winner}!"
+      elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+end
   
     
  
